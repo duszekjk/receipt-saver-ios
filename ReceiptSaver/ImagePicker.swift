84203b97f87f2ea2b4ebcbba2484f19079ddc8c2
Jacek Kałużny <45168146+duszekjk@users.noreply.github.com>
2026-06-18 19:29:42 +0200

    Add document scanner and real grayscale preprocessing

--- ReceiptSaver/ImagePicker.swift
+++ ReceiptSaver/ImagePicker.swift
@@ -1,5 +1,6 @@
 import SwiftUI
 import UIKit
+import VisionKit
 
 struct ImagePicker: UIViewControllerRepresentable {
     enum Source {
@@ -46,8 +47,62 @@ struct ImagePicker: UIViewControllerRepresentable {
     }
 }
 
+struct DocumentScannerView: UIViewControllerRepresentable {
+    let onImage: (UIImage) -> Void
+    let onCancel: () -> Void
+
+    static var isAvailable: Bool {
+        VNDocumentCameraViewController.isSupported
+    }
+
+    func makeUIViewController(context: Context) -> VNDocumentCameraViewController {
+        let scanner = VNDocumentCameraViewController()
+        scanner.delegate = context.coordinator
+        return scanner
+    }
+
+    func updateUIViewController(_ uiViewController: VNDocumentCameraViewController, context: Context) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(onImage: onImage, onCancel: onCancel)
+    }
+
+    final class Coordinator: NSObject, VNDocumentCameraViewControllerDelegate {
+        let onImage: (UIImage) -> Void
+        let onCancel: () -> Void
+
+        init(onImage: @escaping (UIImage) -> Void, onCancel: @escaping () -> Void) {
+            self.onImage = onImage
+            self.onCancel = onCancel
+        }
+
+        func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFinishWith scan: VNDocumentCameraScan) {
+            guard scan.pageCount > 0 else {
+                controller.dismiss(animated: true)
+                onCancel()
+                return
+            }
+            let image = scan.imageOfPage(at: 0).preprocessedForReceipt()
+            controller.dismiss(animated: true) { self.onImage(image) }
+        }
+
+        func documentCameraViewControllerDidCancel(_ controller: VNDocumentCameraViewController) {
+            controller.dismiss(animated: true) { self.onCancel() }
+        }
+
+        func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFailWithError error: Error) {
+            controller.dismiss(animated: true) { self.onCancel() }
+        }
+    }
+}
+
 extension UIImage {
-    func preprocessedForReceipt(maxDimension: CGFloat = 1600) -> UIImage {
+    func preprocessedForReceipt(maxDimension: CGFloat = 1400) -> UIImage {
+        let resized = resizedKeepingAspect(maxDimension: maxDimension)
+        return resized.grayscaleImage() ?? resized
+    }
+
+    private func resizedKeepingAspect(maxDimension: CGFloat) -> UIImage {
         let scale = min(maxDimension / max(size.width, size.height), 1)
         let newSize = CGSize(width: size.width * scale, height: size.height * scale)
         UIGraphicsBeginImageContextWithOptions(newSize, true, 1.0)
@@ -55,5 +110,25 @@ extension UIImage {
         let resized = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         return resized ?? self
+    }
+
+    private func grayscaleImage() -> UIImage? {
+        guard let cgImage = self.cgImage else { return nil }
+        let width = cgImage.width
+        let height = cgImage.height
+        let colorSpace = CGColorSpaceCreateDeviceGray()
+        guard let context = CGContext(
+            data: nil,
+            width: width,
+            height: height,
+            bitsPerComponent: 8,
+            bytesPerRow: width,
+            space: colorSpace,
+            bitmapInfo: CGImageAlphaInfo.none.rawValue
+        ) else { return nil }
+        context.interpolationQuality = .medium
+        context.draw(cgImage, in: CGRect(x: 0, y: 0, width: width, height: height))
+        guard let grayCGImage = context.makeImage() else { return nil }
+        return UIImage(cgImage: grayCGImage, scale: 1.0, orientation: imageOrientation)
     }
 }
