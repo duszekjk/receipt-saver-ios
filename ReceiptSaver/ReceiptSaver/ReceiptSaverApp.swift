2cd8ba7c0cc1bbb9a0bf89dee054611ed36448e6
duszekjk <jacek@duszekjk.com>
2026-06-18 21:33:36 +0200

    info fix

--- ReceiptSaver/ReceiptSaver/ReceiptSaverApp.swift
+++ ReceiptSaver/ReceiptSaver/ReceiptSaverApp.swift
@@ -1,17 +1,10 @@
-//
-//  ReceiptSaverApp.swift
-//  ReceiptSaver
-//
-//  Created by Jacek Kałużny on 6/18/26.
-//
-
 import SwiftUI
 
 @main
 struct ReceiptSaverApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            DashboardView()
         }
     }
 }
